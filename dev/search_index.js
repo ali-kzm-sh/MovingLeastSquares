0cdd0dcc868b0dd8c7d6e9c120047fee2683832a
Documenter.jl <documenter@juliadocs.github.io>
2024-03-17 00:28:11 +0000

    build based on 355e653

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MovingLeastSquares","category":"page"},{"location":"#MovingLeastSquares","page":"Home","title":"MovingLeastSquares","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MovingLeastSquares.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MovingLeastSquares]","category":"page"}]
+}
